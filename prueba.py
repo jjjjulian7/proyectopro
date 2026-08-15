705eeba9ce24667b631d8be685e269e8c9432cf1
julian <jcorrea21@alumnos.utalca.cl>
2026-08-15 16:18:54 -0400

    clase producto inicio

--- prueba.py
+++ prueba.py
@@ -1,2 +1,9 @@
-
-####el weso
+class Producto:
+    def __init__(self, id, nombre, precio, stock, categoria):
+        self.id = id
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+        self.categoria = categoria
+    def mostrar_info(self):
+        print(self.id, self.nombre, self.precio)
