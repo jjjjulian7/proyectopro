271092c6af3b0e15e64f42779e94e425555d0cc6
julian <jcorrea21@alumnos.utalca.cl>
2026-08-16 16:22:27 -0400

    una cosilla para el martes

--- clases/inventario.py
+++ clases/inventario.py
@@ -1,3 +1,3 @@
-class inventario:
+class Inventario:
     def __init__(self):
-        pass
+        self.productos = []
--- clases/profucto.py
+++
@@ -1,9 +0,0 @@
-class Producto:
-    def __init__(self, id, nombre, precio, stock, categoria):
-        self.id = id
-        self.nombre = nombre
-        self.precio = precio
-        self.stock = stock
-        self.categoria = categoria
-    def mostrar_info(self):
-        print(self.id, self.nombre, self.precio)
