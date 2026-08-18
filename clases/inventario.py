906078029fe2cbc287c2fd15722a77dfb7468b4e
jjjjulian7 <jcorrea21@alumnos.utalca.cl>
2026-08-18 18:15:50 -0400

    avance clases

--- clases/inventario.py
+++ clases/inventario.py
@@ -1,3 +1,32 @@
+from datos.bd import bd
+
+
 class Inventario:
     def __init__(self):
         self.productos = []
+
+
+def agregar_producto(self, p):
+    nuevo_producto = (p.id , p.nombre, p.nombre, p.precio, p.stock, p.categoria)
+    self.productos.append(nuevo_producto)
+    bd.insertar_producto()
+
+def buscar_producto(self, nombre, id):
+    print("Seleccione una opción de busqueda")    
+    A = int(input("1 | Busqueda por id       2 | Busqueda por nombre"))
+    if A == 1:
+        id_search = int(input("Ingrese el id del producto"))
+        for p in self.productos:
+            if id_search == p.id:
+                return p
+    if A == 2:
+        productos_buscados = []
+        nombre_search = input("Ingrese el nombre del producto")
+        for p in self.productos:
+            if nombre_search.lower() in p.nombre.lower():
+                productos_buscados.append(p)
+        return productos_buscados
+    
+                
+            
+
