2af4195346791604671a804f220b732b1f4fdff7
JoseCofreV <stemenjose@gmail.com>
2026-08-26 22:53:01 -0400

    login admin listo + limpieza de ventana al cambiar de ventana

--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -1,37 +1,45 @@
 import tkinter as tk
 from . import estilo_boton
 from . import FuncionBotones as F
+from .ventana_admin import abrir_ventana_funcionario
+
+# Codigo realizado por Jose Cofre
+def ir_a_admin(event):
+    ventana.withdraw() # Oculta la ventana principal (Guardamos lo que tenia)
+    abrir_ventana_funcionario() # Abre la ventana de funcionario
+
 
 #ventana de ingreso
 ventana = tk.Tk()
 ventana.title("MaulencitosMarket")
-ventana.geometry("350x350")
-
+ventana.geometry("1280x720")
+#   AJUSTES DE LA VENTANA Y POSICIONAMENTO DE LOS ELEMENTOSSSSSSSSSSSSSSSSSSSSSSSSSS
 # Codigo realizado por Jose Cofre
 texto = tk.Label(ventana, text="Nombre Usuario")
-texto.grid(row=2, column=2, padx=(40, 0), pady=(40, 0))
-
-IngresoNombre = tk.Entry(ventana)
-IngresoNombre.grid(row=2, column=3, padx=(10, 40), pady=(40, 0))
+texto.grid(row=3, column=2, padx=(300, 0), pady=(210, 0))
 
 texto = tk.Label(ventana, text="Clave Usuario")
-texto.grid(row=3, column=2, padx=(40, 0), pady=(10, 0))
+texto.grid(row=4, column=2, padx=(300, 0), pady=(20, 0))
+
+IngresoNombre = tk.Entry(ventana)
+IngresoNombre.grid(row=3, column=3, padx=(10, 300), pady=(210, 0))
 
 IngresoClave = tk.Entry(ventana, show="*")
-IngresoClave.grid(row=3, column=3, padx=(10, 40), pady=(10, 0))
+IngresoClave.grid(row=4, column=3, padx=(10, 300), pady=(20, 0))
 
 
 # Codigo realizado por Jose Cofre
 #botones,llamamos al archivo estilo_boton
-boton_registrar = estilo_boton.crear_boton_verde(ventana, "Registrar",lambda:F.registro(IngresoNombre,IngresoClave,ventana)) 
-boton_registrar.grid(row=4, column=2, padx=(40, 10), pady=(20, 0))
+boton_registrar = estilo_boton.crear_boton_verde(ventana, "Registrar",lambda:F.simular_registro(IngresoNombre,IngresoClave,ventana)) 
+boton_registrar.grid(row=5, column=2, padx=(300, 10), pady=(50, 0))
 
-boton_ingresar = estilo_boton.crear_boton_verde(ventana, "Ingresar",lambda: F.ventana_usuario(ventana,IngresoNombre,IngresoClave)) 
-boton_ingresar.grid(row=4, column=3, padx=(10, 40), pady=(20, 0))
+boton_ingresar = estilo_boton.crear_boton_verde(ventana, "Ingresar",lambda: F.ventana_usuario(ventana)) 
+boton_ingresar.grid(row=5, column=3, padx=(10, 300), pady=(50, 0))
 
-#texto para cambiar al login del admin
+
 cambiar_admin= tk.Label(ventana, text="¿Eres administrador? Haz click aqui")
-cambiar_admin.grid(row=5, column=2, padx=(0,0), pady=(170, 0))
-cambiar_admin.bind("<Button-1>",lambda event:F.ventana_admin(ventana))
+cambiar_admin.grid(row=12, column=0, padx=(0,0), pady=(320, 0))
+# Ya estaba bien, llama a la función que importamos arriba
+cambiar_admin.bind("<Button-1>",ir_a_admin,lambda event: abrir_ventana_funcionario())
 
 ventana.mainloop()
