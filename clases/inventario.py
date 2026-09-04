014a8d03a10c81003b0c5d8cd03a052e19f9bdbe
sleon00 <seba.encaladaaa@gmail.com>
2026-09-04 00:35:15 -0400

    Arreglo de imagen y de buscador

--- clases/inventario.py
+++ clases/inventario.py
@@ -53,8 +53,10 @@ class Inventario:
                     return p
         elif opcion == 2:
             productos_buscados = []
+            terminos = valor.lower().split()
             for p in self.productos:
-                if valor.lower() in p.nombre.lower() or valor.lower() in p.categoria.lower():
+                texto_producto = f"{p.nombre} {p.categoria}".lower()
+                if all(termino in texto_producto for termino in terminos):
                     productos_buscados.append(p)
             return productos_buscados
         return None
